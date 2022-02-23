52aaf8264fe58a3b2db7f0bc181e98074483710d
DanielKohn1208 <danielmkohn1208@gmail.com>
2022-02-22 21:38:44 -0500

    added navbar to its own component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="#">Covid Stats Canada</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-					<li class="nav-item">
-						<router-link class="nav-link active" to="/">Home</router-link> 
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+	<Navbar />
   <body class="m-5">
 		<router-view/>
   </body>
 
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+
+export default{
+    name: "App",
+    compoenents: {
+        Navbar
+    },
+    components: { Navbar }
+}
+</script>
