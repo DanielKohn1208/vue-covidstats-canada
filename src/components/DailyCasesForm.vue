a186a187f8b624c61efc668eb51f708d9a4771ec
DanielKohn1208 <danielmkohn1208@gmail.com>
2022-02-25 22:38:47 -0500

    added credit to api being used, added way to get get covid stats canada wide on any given day, the store may need to be refactored as their is quite a bit of spaghetti code

---
+++ src/components/DailyCasesForm.vue
@@ -0,0 +1,62 @@
+<template>
+	<form @submit="onSubmit" class="row row-cols-lg-auto g-3">
+			<div class="col">
+				<input v-model="searchDay" type="number" min="1" max="31" class="form-control" id="day" placeholder="Day">
+			</div>
+			<div class="col">
+				<select v-model="searchYear" class="form-select" id="month" aria-label="Default select example">
+					<option selected>Year</option>
+					<option value="2020">2020</option>
+					<option value="2021">2021</option>
+					<option value="2022">2022</option>
+				</select>
+			</div>
+			<div class="col">
+				<select v-model="searchMonth" id="month" class="form-select" aria-label="Default select example">
+					<option selected>Month</option>
+					<option value="01">January</option>
+					<option value="02">February</option>
+					<option value="03">March</option>
+					<option value="04">April</option>
+					<option value="05">May</option>
+					<option value="06">June</option>
+					<option value="07">July</option>
+					<option value="08">August</option>
+					<option value="09">September</option>
+					<option value="10">October</option>
+					<option value="11">November</option>
+					<option value="12">December</option>
+				</select>
+			</div>
+			<div class="mb-3">
+				<button type="submit" class="btn btn-primary mb-3">Get Stats</button>
+			</div>
+	</form>
+</template>
+
+<script>
+export default {
+	name:"DailyCasesForm",
+	data(){
+		return {
+			searchYear: "Year",
+			searchMonth: "Month",
+			searchDay: String
+		}
+	},
+	methods: {
+		onSubmit(e){
+			e.preventDefault();
+			var dateString = "";
+			if(this.searchDay < 10){
+				dateString= `${this.searchYear}-${this.searchMonth}-0${this.searchDay}`
+			}else{
+				dateString= `${this.searchYear}-${this.searchMonth}-${this.searchDay}`
+			}
+			console.log(dateString);
+			this.$store.dispatch('getDayCasesDate',dateString);
+		}
+	},	
+}
+	
+</script>
---
+++ src/views/CasesView.vue
@@ -0,0 +1,23 @@
+<template>
+	<h1>
+		This is the cases page
+	</h1>
+	<DailyCasesForm />
+
+	<p>Total cases for {{this.$store.getters.getDate}}: {{this.$store.state.dayCases}}</p>
+</template>
+
+<script>
+import DailyCasesForm from '../components/DailyCasesForm.vue';
+
+export default{
+	name: 'HomeView',
+	components: {
+		DailyCasesForm,
+	},
+	mounted() {
+		this.$store.dispatch('getDayCasesInitial')
+	}
+
+}
+</script>
